9072fa1cebec286fec6d9e175a7122fbe1a7f875
Subithasuyambu <subithssuysmbu31@gmail.com>
2024-03-11 10:45:11 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Route,Routes} from 'react-router-dom';
+import { Box } from '@mui/material';
+import ExerciseDetail from './Pages/ExerciseDetail';
+import Home from './Pages/Home';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+
+import './App.css'
+
+export const App = () => {
+  return (
+    <Box width="400px" sx={{ width: { x1:'1488px'}}} m='auto'>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer></Footer>
+      
+    </Box>
+  )
+}
+export default App;
---
+++ src/Pages/Home.js
@@ -0,0 +1,28 @@
+import React,{useState} from 'react';
+import{Box} from '@mui/material';
+import Exercises from '../Components/Exercises';
+import HeroBanner from '../Components/HeroBanner';
+import SearchExercises from '../Components/SearchExercises';
+
+export const Home = () => {
+  const [bodyPart,setBodyPart]=useState('all')
+  const [exercises,setExercises]=useState([]);
+   //console.log(bodyPart);
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises  
+      excercise={exercises}
+      setExercises={setExercises}
+      bodyPart={bodyPart} 
+      setBodyPart={setBodyPart}
+      />
+      <Exercises   
+      exercises={exercises}
+      setExercises={setExercises}
+      bodyPart={bodyPart} 
+     />
+      </Box>
+  )
+}
+export default Home;
